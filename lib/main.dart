9bf73d2b805a3d31b6c482e461c1a0027fffe10f
ADITHYA_ANIL_B20CS1206 <adithyaanil.b20cs1206@mbcet.ac.in>
2023-07-18 06:54:04 +0000

    aboutpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,164 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AboutUsPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        color: Color(0xFF2B2930), // Updated background color here
+        padding: EdgeInsets.fromLTRB(42, 32, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'About us',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 43,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFFFEF7FF), // Updated font color here
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Meet the creators',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFFFEF7FF), // Updated font color here
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProfileWidget(
+                  imagePath: 'assets/profile1.jpg',
+                  name: 'Adithya Anil',
+                  email: 'unidreamerzz@gmail.com',
+                ),
+                SizedBox(width: 10),
+                ProfileWidget(
+                  imagePath: 'assets/profile2.jpg',
+                  name: 'Ashwin A',
+                  email: 'ashwin022002@gmail.com',
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProfileWidget(
+                  imagePath: 'assets/profile3.jpg',
+                  name: 'Gopal S',
+                  email: 'gopalshibu142@gmail.com',
+                ),
+                SizedBox(width: 10),
+                ProfileWidget(
+                  imagePath: 'assets/profile4.jpg',
+                  name: 'Shibin Shibu',
+                  email: 'shibinsb01@gmail.com',
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Guided by',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFFFEF7FF), // Updated font color here
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProfileWidget(
+                  imagePath: 'assets/profile5.jpg',
+                  name: 'Ms Deepthi K Moorthy',
+                  email: 'Assistant professor, Dept. of CSE',
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  final String imagePath;
+  final String name;
+  final String email;
+  final double circleSize;
+
+  const ProfileWidget({
+    required this.imagePath,
+    required this.name,
+    required this.email,
+    this.circleSize = 83.53,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: circleSize,
+          height: circleSize,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.amber, // Dark yellow border color
+              width: 3,
+            ),
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          name,
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFFFEF7FF), // Updated font color here
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          email,
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 7,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFFFEF7FF), // Updated font color here
+          ),
+        ),
+      ],
     );
   }
 }
